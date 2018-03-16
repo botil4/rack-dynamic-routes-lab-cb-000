8c5eed0a030ed4b582f558499927f449435f249b
Travis Clarkson <tk1667@gmail.com>
2018-03-16 00:27:16 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/items/)
 
       item_name = req.path.split("/songs/").last
-      item_found = @@item.find{|item| item.name == item_name} 
+      item_found = @@item.find{|item| item.name == item_name}
 
       resp.write item_found.price
     else
